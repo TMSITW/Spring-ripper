894812d26c58e642b62dbccee732d0f21f58d28e
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-08-26 21:47:17 +0300

    3'th commit

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,28 @@
 package main.java.quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i <repeat; i++) {
+            System.out.println("message = " + message);
+        }
     }
 
     public void setMessage(String message) {
