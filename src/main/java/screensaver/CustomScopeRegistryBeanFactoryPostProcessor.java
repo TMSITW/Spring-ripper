f48cb68372f7223d8bedf07b9598462c47439c84
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-08-26 23:42:54 +0300

    8'th commit

---
+++ src/main/java/screensaver/CustomScopeRegistryBeanFactoryPostProcessor.java
@@ -0,0 +1,15 @@
+package main.java.screensaver;
+
+import main.java.screensaver.PeriodicalScopeConfigurer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomScopeRegistryBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        configurableListableBeanFactory.registerScope("periodical", new PeriodicalScopeConfigurer());
+    }
+}
