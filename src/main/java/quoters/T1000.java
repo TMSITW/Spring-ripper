a3b7e2c43a7b7d0c62a148f5332e263584fbd55c
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-08-26 22:51:03 +0300

    5'th commit

---
+++ src/main/java/quoters/T1000.java
@@ -0,0 +1,8 @@
+package main.java.quoters;
+
+public class T1000 extends TerminatorQuoter implements Quoter{
+    @Override
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
