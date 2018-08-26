2b47a9a2457bd6c7d4c86d836426722ccdbc6c62
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-08-26 23:22:16 +0300

    6'th commit

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,27 @@
+package main.java.screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Service
+public class ColorFrame extends JFrame {
+
+    @Autowired
+    private Color color;
+    public ColorFrame() {
+        setSize(200,200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random=new Random();
+        setLocation(random.nextInt(1980), random.nextInt(1020));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+}
